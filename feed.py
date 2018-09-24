5c59d5c5af6ae4586ce9b65e7724f8aab6034156
Fionn Fitzmaurice <iamfionn@github.com>
2018-09-25 01:24:42 +0800

    fix bugs and improve feed content
    * get title from blog
    * add snippet property
    * description now returns description metadata
    * better date parsing
    * get article content
    * optional arguments for feed class
    * apply epoch date properly
    * pretty-print xml

--- feed.py
+++ feed.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 import datetime
+import html
 from typing import Union, List
 import requests
 from feedgen.feed import FeedGenerator
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 class Blog:
 
@@ -12,6 +13,7 @@ class Blog:
         self._html = self._get(url)
         self._soup = BeautifulSoup(self._html, features="lxml")
         self.articles = self._articles()
+        self.title = self._soup.title.string
 
     @staticmethod
     def _get(url: str) -> bytes:
@@ -33,7 +35,7 @@ class Article:
         self.title = title
         self._html = self._get(url)
         self._soup = BeautifulSoup(self._html, features="lxml")
-        self.description = self._soup.article.p.text.strip() + "…"
+        self.snippet = self._soup.body.article.p.text.strip() + " […]"
 
     @staticmethod
     def _get(url: str) -> bytes:
@@ -44,28 +46,48 @@ class Article:
     @property
     def date(self: Article) -> Union[datetime.datetime, None]:
         try:
-            date = [int(x) for x in self._soup.time.get("datetime").split("-")]
-            return datetime.datetime(*date, tzinfo=datetime.timezone(datetime.timedelta()))
+            date = datetime.datetime.fromisoformat(self._soup.time.get("datetime"))
+            return date.replace(tzinfo=datetime.timezone.utc)
         except AttributeError:
             return None
 
+    @property
+    def description(self) -> Union[None, str]:
+        try:
+            return self._soup.find(attrs={"name": "description"}).get("content")
+        except AttributeError:
+            return None
+
+    @property
+    def content(self):
+        content = []
+        initial_p = self._soup.body.article.find("p")
+        content.append(html.escape(str(initial_p), quote=True))
+        for tag in initial_p.next_siblings:
+            if isinstance(tag, Tag):
+                content.append(html.escape(str(tag), quote=True))
+        return "\n".join(content)
+
 class Feed:
 
-    def __init__(self: Feed, title: str, url: str, name: str, email: str,
-                 generator: str, generator_version: str, logo: str, icon: str,
-                 description: str, language: str) -> None:
+    def __init__(self: Feed, url: str, name: str, email: str, title: str = None,
+                 generator: str = None, generator_version: str = None,
+                 logo: str = None, icon: str = None, description: str = None,
+                 language: str = None) -> None:
         self.name = name
         self.email = email
+
         self.fg = FeedGenerator()
-        self.fg.title(title)
         self.fg.id(url + "feed.atom")
-        self.fg.author(name=name, email=email)
-        self.fg.managingEditor(email)
-        self.fg.webMaster(email)
-        self.fg.contributor(name=name, email=email)
-        self.fg.generator(generator=generator, version=generator_version)
         self.fg.link(href=url + "feed.xml", rel="self")
         self.fg.link(href=url, rel="alternate")
+        self.fg.author(name=name, email=email)
+        self.fg.contributor(name=name, email=email)
+        self.fg.managingEditor(email)
+        self.fg.webMaster(email)
+
+        self.fg.title(title)
+        self.fg.generator(generator=generator, version=generator_version)
         self.fg.logo(logo)
         self.fg.icon(icon)
         self.fg.description(description)
@@ -78,17 +100,21 @@ class Feed:
         feed_entry.link(href=article.url)
         feed_entry.guid(guid=article.url, permalink=True)
         feed_entry.author(name=self.name, email=self.email)
-        feed_entry.summary(article.description)
+        feed_entry.summary(article.description or article.snippet)
+        feed_entry.content(content=article.content)
         feed_entry.published(article.date)
         if article.date:
+            feed_entry.published(article.date)
             feed_entry.updated(article.date)
         else:
-            utc = datetime.timezone(datetime.timedelta())
-            epoch = datetime.datetime(1970, 1, 1, tzinfo=utc)
+            epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+            feed_entry.published(epoch)
             feed_entry.updated(epoch)
 
     def add_from_blog(self: Feed, url: str) -> None:
         blog = Blog(url)
+        if not self.fg.title():
+            self.fg.title(blog.title)
         for article in blog.articles:
             self.add(article)
 
@@ -99,7 +125,7 @@ class Feed:
         return self.fg.rss_str(pretty=True)
 
     def atom_file(self: Feed, filename: str = "feed.atom") -> None:
-        self.fg.atom_file(filename)
+        self.fg.atom_file(filename, pretty=True)
 
     def rss_file(self: Feed, filename: str = "feed.xml") -> None:
-        self.fg.rss_file(filename)
+        self.fg.rss_file(filename, pretty=True)
