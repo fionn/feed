0284342fe40b38465d247bbfd16fa06d57403954
Fionn Fitzmaurice <iamfionn@github.com>
2018-09-25 01:46:23 +0800

    remove forward declarations, specify content type

--- feed.py
+++ feed.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 import datetime
 import html
 from typing import Union, List
@@ -8,7 +7,7 @@ from bs4 import BeautifulSoup, Tag
 
 class Blog:
 
-    def __init__(self: Blog, url: str) -> None:
+    def __init__(self, url: str) -> None:
         self.url = url
         self._html = self._get(url)
         self._soup = BeautifulSoup(self._html, features="lxml")
@@ -21,7 +20,7 @@ class Blog:
         response.raise_for_status()
         return response.content
 
-    def _articles(self: Blog) -> List[Article]:
+    def _articles(self) -> List["Article"]:
         articles = []
         for element in self._soup.main.ul.find_all("li"):
             article_url = self.url + element.a.get("href")
@@ -30,7 +29,7 @@ class Blog:
 
 class Article:
 
-    def __init__(self: Article, title: str, url: str) -> None:
+    def __init__(self, title: str, url: str) -> None:
         self.url = url
         self.title = title
         self._html = self._get(url)
@@ -44,7 +43,7 @@ class Article:
         return response.content
 
     @property
-    def date(self: Article) -> Union[datetime.datetime, None]:
+    def date(self) -> Union[datetime.datetime, None]:
         try:
             date = datetime.datetime.fromisoformat(self._soup.time.get("datetime"))
             return date.replace(tzinfo=datetime.timezone.utc)
@@ -59,7 +58,7 @@ class Article:
             return None
 
     @property
-    def content(self):
+    def content(self) -> str:
         content = []
         initial_p = self._soup.body.article.find("p")
         content.append(html.escape(str(initial_p), quote=True))
@@ -70,7 +69,7 @@ class Article:
 
 class Feed:
 
-    def __init__(self: Feed, url: str, name: str, email: str, title: str = None,
+    def __init__(self, url: str, name: str, email: str, title: str = None,
                  generator: str = None, generator_version: str = None,
                  logo: str = None, icon: str = None, description: str = None,
                  language: str = None) -> None:
@@ -93,7 +92,7 @@ class Feed:
         self.fg.description(description)
         self.fg.language(language)
 
-    def add(self: Feed, article: Article) -> None:
+    def add(self, article: Article) -> None:
         feed_entry = self.fg.add_entry()
         feed_entry.id(article.url)
         feed_entry.title(article.title)
@@ -101,7 +100,7 @@ class Feed:
         feed_entry.guid(guid=article.url, permalink=True)
         feed_entry.author(name=self.name, email=self.email)
         feed_entry.summary(article.description or article.snippet)
-        feed_entry.content(content=article.content)
+        feed_entry.content(content=article.content, type="html")
         feed_entry.published(article.date)
         if article.date:
             feed_entry.published(article.date)
@@ -111,21 +110,21 @@ class Feed:
             feed_entry.published(epoch)
             feed_entry.updated(epoch)
 
-    def add_from_blog(self: Feed, url: str) -> None:
+    def add_from_blog(self, url: str) -> None:
         blog = Blog(url)
         if not self.fg.title():
             self.fg.title(blog.title)
         for article in blog.articles:
             self.add(article)
 
-    def atom(self: Feed) -> bytes:
+    def atom(self) -> str:
         return self.fg.atom_str(pretty=True)
 
-    def rss(self: Feed) -> bytes:
+    def rss(self) -> str:
         return self.fg.rss_str(pretty=True)
 
-    def atom_file(self: Feed, filename: str = "feed.atom") -> None:
+    def atom_file(self, filename: str = "feed.atom") -> None:
         self.fg.atom_file(filename, pretty=True)
 
-    def rss_file(self: Feed, filename: str = "feed.xml") -> None:
+    def rss_file(self, filename: str = "feed.xml") -> None:
         self.fg.rss_file(filename, pretty=True)
